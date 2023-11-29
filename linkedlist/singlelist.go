3802242a62ecd0299a3cdc8de4bacdc008de513f
WeiChen Lin <asdfg09487@gmail.com>
2023-11-29 12:49:19 +0800

    :sparkles: Finish linked list

---
+++ linkedlist/singlelist.go
@@ -0,0 +1,164 @@
+package linkedList
+
+import (
+	"errors"
+	"sync"
+
+	"github.com/google/uuid"
+)
+
+type Node struct {
+	Value uuid.UUID
+	Next  *Node
+}
+
+type SingleLinkedList struct {
+	Head   *Node
+	Length int
+	Lock   sync.Mutex
+}
+
+func (l *SingleLinkedList) Append(value uuid.UUID) {
+	l.Lock.Lock()
+	defer l.Lock.Unlock()
+	node := &Node{Value: value}
+
+	if l.Head == nil {
+		l.Head = node
+	} else {
+		currentNode := l.Head
+
+		for currentNode.Next != nil {
+			currentNode = currentNode.Next
+		}
+
+		currentNode.Next = node
+	}
+
+	l.Length++
+}
+
+func (l *SingleLinkedList) Delete(value uuid.UUID) error {
+	l.Lock.Lock()
+	defer l.Lock.Unlock()
+
+	if l.Head == nil {
+		return errors.New("this single linked list is empty")
+	}
+
+	if l.Head.Value == value {
+		l.Head = l.Head.Next
+		l.Length--
+		return nil
+	}
+
+	currentNode := l.Head
+
+	for currentNode.Next != nil {
+		if currentNode.Next.Value == value {
+			currentNode.Next = currentNode.Next.Next
+			l.Length--
+			return nil
+		}
+
+		currentNode = currentNode.Next
+	}
+
+	return errors.New("the value was not found in this single linked list")
+}
+
+func (l *SingleLinkedList) Prepend(value uuid.UUID) {
+	l.Lock.Lock()
+	defer l.Lock.Unlock()
+	node := &Node{Value: value}
+
+	if l.Head == nil {
+		l.Head = node
+	} else {
+		node.Next = l.Head
+		l.Head = node
+	}
+
+	l.Length++
+}
+
+func (l *SingleLinkedList) Search(value uuid.UUID) (int, error) {
+	l.Lock.Lock()
+	defer l.Lock.Unlock()
+
+	if l.Head == nil {
+		return 0, errors.New("this single linked list is empty")
+	}
+
+	currentNode := l.Head
+	index := 0
+
+	for currentNode != nil {
+		if currentNode.Value == value {
+			return index, nil
+		}
+
+		currentNode = currentNode.Next
+		index++
+	}
+
+	return 0, errors.New("the value was not found in this single linked list")
+}
+
+func (l *SingleLinkedList) ValueOf(index int) (uuid.UUID, error) {
+	l.Lock.Lock()
+	defer l.Lock.Unlock()
+
+	if index < 0 || index > l.Length {
+		return uuid.Nil, errors.New("the index is out of range")
+	}
+
+	currentNode := l.Head
+	currentIndex := 0
+
+	for currentNode != nil {
+		if currentIndex == index {
+			return currentNode.Value, nil
+		}
+
+		currentNode = currentNode.Next
+		currentIndex++
+	}
+
+	return uuid.Nil, errors.New("the index was not found in this single linked list")
+}
+
+func (l *SingleLinkedList) InsertBehindWithIndex(index int, value uuid.UUID) error {
+	l.Lock.Lock()
+	defer l.Lock.Unlock()
+
+	if index < 0 {
+		return errors.New("the index is out of range")
+	}
+
+	if index == 0 {
+		head := l.Head
+		l.Head = &Node{Value: value}
+		l.Head.Next = head
+		l.Length++
+		return nil
+	}
+
+	node := &Node{Value: value}
+
+	currentNode := l.Head
+	currentIndex := 0
+
+	for currentNode != nil {
+		if currentIndex == index {
+			node.Next = currentNode.Next
+			currentNode.Next = node
+			l.Length++
+			return nil
+		}
+
+		currentNode = currentNode.Next
+		currentIndex++
+	}
+	return errors.New("the index was not found in this single linked list")
+}
---
+++ linkedlist/singlelist_test.go
@@ -0,0 +1,260 @@
+package linkedList
+
+import (
+	"sync"
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_Append(t *testing.T) {
+	list := SingleLinkedList{}
+	list.Append(uuid.New())
+
+	require.Equal(t, 1, list.Length)
+}
+
+func Test_Delete_Singel(t *testing.T) {
+	list := SingleLinkedList{}
+
+	id := uuid.New()
+
+	list.Append(id)
+
+	err := list.Delete(id)
+	require.NoError(t, err)
+	require.Equal(t, list.Length, 0)
+}
+
+func Test_Delete_Empty(t *testing.T) {
+	list := SingleLinkedList{}
+	id := uuid.New()
+	err := list.Delete(id)
+	require.Error(t, err)
+	require.Equal(t, list.Length, 0)
+}
+
+func Test_Prepend(t *testing.T) {
+	list := SingleLinkedList{}
+
+	id := uuid.New()
+
+	list.Append(id)
+	require.Equal(t, list.Head.Value, id)
+
+	id_2 := uuid.New()
+	list.Prepend(id_2)
+	require.Equal(t, list.Head.Value, id_2)
+	require.Equal(t, list.Head.Next.Value, id)
+	require.Equal(t, list.Length, 2)
+}
+
+func Test_Prepend_Empty(t *testing.T) {
+	list := SingleLinkedList{}
+
+	id := uuid.New()
+	list.Prepend(id)
+
+	require.Equal(t, list.Head.Value, id)
+	require.Equal(t, list.Length, 1)
+}
+
+func Test_Search(t *testing.T) {
+	list := SingleLinkedList{}
+
+	id := uuid.New()
+	list.Append(id)
+
+	id_2 := uuid.New()
+	list.Append(id_2)
+
+	id_3 := uuid.New()
+	list.Append(id_3)
+
+	index, err := list.Search(id_2)
+	require.NoError(t, err)
+	require.Equal(t, index, 1)
+}
+
+func Test_Search_Empty(t *testing.T) {
+	list := SingleLinkedList{}
+
+	id := uuid.New()
+	_, err := list.Search(id)
+	require.Error(t, err)
+}
+
+func Test_Search_Not_Exist(t *testing.T) {
+	list := SingleLinkedList{}
+
+	id := uuid.New()
+	list.Append(id)
+
+	id_2 := uuid.New()
+	list.Append(id_2)
+
+	id_3 := uuid.New()
+	list.Append(id_3)
+
+	id_4 := uuid.New()
+	_, err := list.Search(id_4)
+	require.Error(t, err)
+}
+
+func Test_ValueOf(t *testing.T) {
+	list := SingleLinkedList{}
+
+	id := uuid.New()
+	list.Append(id)
+
+	id_2 := uuid.New()
+	list.Append(id_2)
+
+	id_3 := uuid.New()
+	list.Append(id_3)
+
+	value, err := list.ValueOf(1)
+	require.NoError(t, err)
+	require.Equal(t, value, id_2)
+
+	_, err = list.ValueOf(3)
+	require.Error(t, err)
+
+	_, err = list.ValueOf(-1)
+	require.Error(t, err)
+}
+
+func Test_ValueOf_Empty(t *testing.T) {
+	list := SingleLinkedList{}
+
+	_, err := list.ValueOf(0)
+	require.Error(t, err)
+}
+
+
+func Test_Concurrency(t *testing.T) {
+	list := SingleLinkedList{}
+
+	wg := sync.WaitGroup{}
+
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			list.Append(uuid.New())
+		}()
+	}
+
+	wg.Wait()
+
+	require.Equal(t, 1000, list.Length)
+}
+
+func Test_Insert_Prepend(t *testing.T) {
+	list := SingleLinkedList{}
+
+	id := uuid.New()
+	list.Append(id)
+
+	id_2 := uuid.New()
+	list.Append(id_2)
+
+	id_3 := uuid.New()
+	list.Append(id_3)
+
+	id_4 := uuid.New()
+	err := list.InsertBehindWithIndex(0, id_4)
+	require.NoError(t, err)
+
+	require.Equal(t, list.Length, 4)
+	require.Equal(t, list.Head.Value, id_4)
+
+	err = list.InsertBehindWithIndex(-1, id_4)
+	require.Error(t, err)
+
+	err = list.InsertBehindWithIndex(list.Length + 1, id_4)
+	require.Error(t, err)
+}
+
+func Test_Insert_Append(t *testing.T) {
+	list := SingleLinkedList{}
+
+	id := uuid.New()
+	list.Append(id)
+
+	id_2 := uuid.New()
+	list.Append(id_2)
+
+	id_3 := uuid.New()
+	list.Append(id_3)
+
+	id_4 := uuid.New()
+	err := list.InsertBehindWithIndex(list.Length-1, id_4)
+	require.NoError(t, err)
+
+	id_append, err := list.ValueOf(list.Length - 1)
+	require.NoError(t, err)
+
+	require.Equal(t, list.Length, 4)
+	require.Equal(t, id_append, id_4)
+}
+
+func Test_Insert_Middel(t *testing.T){
+	list := SingleLinkedList{}
+
+	id := uuid.New()
+	list.Append(id)
+
+	id_2 := uuid.New()
+	list.Append(id_2)
+
+	id_3 := uuid.New()
+	list.Append(id_3)
+
+	id_4 := uuid.New()
+	err := list.InsertBehindWithIndex(1, id_4)
+	require.NoError(t, err)
+
+	id_append, err := list.ValueOf(2)
+	require.NoError(t, err)
+
+	require.Equal(t, list.Length, 4)
+	require.Equal(t, id_append, id_4)
+}
+
+func Test_Delete(t *testing.T){
+	list := SingleLinkedList{}
+
+	id := uuid.New()
+	list.Append(id)
+
+	id_2 := uuid.New()
+	list.Append(id_2)
+
+	id_3 := uuid.New()
+	list.Append(id_3)
+
+	err := list.Delete(id_2)
+	require.NoError(t, err)
+
+	require.Equal(t, list.Length, 2)
+	require.Equal(t, list.Head.Next.Value, id_3)
+}
+
+func Test_Delete_Not_EXIST(t *testing.T){
+	list := SingleLinkedList{}
+
+	id := uuid.New()
+	list.Append(id)
+
+	id_2 := uuid.New()
+	list.Append(id_2)
+
+	id_3 := uuid.New()
+	list.Append(id_3)
+
+	id_4 := uuid.New()
+	err := list.Delete(id_4)
+	require.Error(t, err)
+}
